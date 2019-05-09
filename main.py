661576213f4ece5ef734c87a94ecd25c8dd1d248
hnoraa <aaron.horeth.e@gmail.com>
2019-05-09 17:07:58 -0400

    Removed magic numbers

--- main.py
+++ main.py
@@ -6,6 +6,12 @@ endDate = None
 startDate = datetime.now()
 dateDiff = 0
 localMode = False
+port = 9090
+host = '0.0.0.0'
+
+# date formats
+fullDate = '%m/%d/%Y %H:%M:%S'
+smallDate = '%Y-%m-%d'
 
 # instantiate Flask framework
 app = Flask(__name__)
@@ -18,14 +24,14 @@ def index():
     global dateDiff
 
     # set params and format things
-    start = startDate.strftime('%m/%d/%Y %H:%M:%S')
+    start = startDate.strftime(fullDate)
     end = endDate
     diff = dateDiff
 
     if endDate is not None:
         startDate = datetime.now()
         dateDiff = endDate - startDate
-        end = endDate.strftime('%m/%d/%Y %H:%M:%S')
+        end = endDate.strftime(fullDate)
     
     # render template
     return render_template('index.html', start=start, end=end, diff=diff)
@@ -42,7 +48,7 @@ def getDateRange():
 
     # end date, if date time is not set in post body
     if request.form['end-date'] is not '' and request.form['end-date'] is not None and len(request.form['end-date']) > 0:
-        endDate = datetime.strptime(request.form['end-date'], '%Y-%m-%d')
+        endDate = datetime.strptime(request.form['end-date'], smallDate)
         endDate.replace(minute=0, hour=0, second=0, microsecond=0)
         endDate = endDate + timedelta(hours=7)
 
@@ -56,7 +62,7 @@ if __name__ == '__main__':
     # RUN!!!!!
     if localMode == False:
         # allow others to connect
-        app.run(host='0.0.0.0', port=9090)
+        app.run(host=host, port=port)
     else:
         # run on localhost
-        app.run(port=9090)
+        app.run(port=port)
